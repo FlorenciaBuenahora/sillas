a6821d4bb47536d24f169feb975d1f4706b81587
Florencia Buenahora <florencia.buenahora@outlook.com>
2021-06-19 17:38:28 -0300

    footer

--- index.php
+++ index.php
@@ -135,6 +135,8 @@ $resultDestacados=mysqli_query($link, $queryDestacados);
         </div>
     </section>
 </main>
+<?php include "footer.php";?>
+
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="./js/carousel.js"></script>
